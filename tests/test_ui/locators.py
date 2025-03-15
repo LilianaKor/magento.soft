c0f15da40950b05f9cf6eb069e1d1c19d33822a2
Liliana <lilianakoric@gmail.com>
2025-03-14 20:12:20 -0700

    Added locators.py, test_outh_positive.py improve with screen shot

---
+++ tests/test_ui/locators.py
@@ -0,0 +1,6 @@
+class RegistrationPageLocators:
+    EMAIL_FIELD = 'input#email_address'
+    PASSWORD_FIELD = 'input#password'
+    CONFIRM_PASSWORD_FIELD = 'input#password-confirmation'
+    CREATE_ACCOUNT_BTN = 'button[title="Create an Account"]'
+    ERROR_MESSAGE = 'div[class="message-error error message"]'
