8b59123ce89b55a9e62ef8f3ed3edff3128f2389
Liliana <lilianakoric@gmail.com>
2025-03-27 09:35:16 -0700

    Added locators.py, need to improve with screenshot

--- tests/test_ui/test_firstLK.py
+++ tests/test_ui/test_firstLK.py
@@ -1,7 +1,24 @@
 import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+# def test_negative_email_validation(driver):
+#     browser.get(f"{custom_base_url}/customer/account/create")
+#     browser.find_element('xpath', "//input[@id='firstname']").send_keys("Lady")
+#     browser.find_element(By.XPATH, "//input[@id='lastname']").send_keys("GAGAha")
+#     email_field = wait.until(EC.presence_of_element_located((By.ID, "email_address")))
+    # Вводим некорректный email
+    # driver.find_element(By.CSS_SELECTOR, Locators.EMAIL_FIELD).send_keys("invalid_email")
+    # driver.find_element(By.CSS_SELECTOR, Locators.PASSWORD_FIELD).send_keys("Test12345!")
+    # driver.find_element(By.CSS_SELECTOR, Locators.CONFIRM_PASSWORD_FIELD).send_keys("Test12345!")
+    # driver.find_element(By.CSS_SELECTOR, Locators.CREATE_ACCOUNT_BTN).click()
+
+    # Проверяем, что появилось сообщение об ошибке
+    # error_message = driver.find_element(By.CSS_SELECTOR, Locators.ERROR_MESSAGE).text
+    # assert "Please enter a valid email address" in error_message
 
 
 def test_outh_positive(browser, custom_base_url, wait, faker_data):
@@ -19,3 +36,6 @@ def test_outh_positive(browser, custom_base_url, wait, faker_data):
     wait.until(EC.url_contains("/customer/account"))
 
     assert "/customer/account" in browser.current_url, "URL is not correct"
+
+
+
